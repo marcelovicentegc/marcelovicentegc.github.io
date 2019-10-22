b48961a1000ef98207a94802b173e49fe81f2029
Travis CI User <travis@example.org>
2019-10-22 03:39:23 +0000

    Updates

--- component---src-pages-page-2-tsx-604a1c912a0ced2e9435.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{211:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(76),c=a(209),r=a(208);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-tsx-604a1c912a0ced2e9435.js.map
